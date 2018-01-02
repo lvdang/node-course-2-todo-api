6967b281e28a18b902ca1d6b6fbe3fd1f916580b
luandang <luan.dang@illumio.com>
2018-01-01 16:19:45 -0800

    Add GET /todos

--- server/server.js
+++ server/server.js
@@ -23,6 +23,15 @@ app.post('/todos', (req, res) => {
   })
 });
 
+
+app.get('/todos', (req, res) => {
+  Todo.find().then( (todos) => {
+    res.send({todos});
+  }, (e) => {
+    res.status(404).send(e);
+  })
+});
+
 app.listen(3000, () => {
   console.log('SERVER STARTED');
 });
